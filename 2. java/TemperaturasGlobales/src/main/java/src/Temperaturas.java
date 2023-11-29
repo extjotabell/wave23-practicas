5db5ffb7e2a94bb062795379096f62e6c9189aa7
AlimpioDiaz <andres.limpiodiaz@mercadolibre.com.mx>
2023-11-29 12:21:07 -0600

    Adding live task -Temperaturas Globales- from -Introducción a Java P1-

---
+++ 2. java/TemperaturasGlobales/src/main/java/src/Temperaturas.java
@@ -0,0 +1,36 @@
+package src;
+
+public class Temperaturas {
+    public static void main(String[] args) {
+
+        String [] ciudades = {"Londres", "Madrid", "Nueva York", "Buenos Aires", "Asunción", "São Paulo",
+                "Lima", "Santiago de Chile", "Lisboa", "Tokio"};
+
+        int [][] temperatura = {{-2, 33}, {-3, 32}, {-8, 27}, {4, 37}, {6, 42}, {5, 43},
+                {0, 39}, {-7, 26}, {-1, 31}, {-10, 35}};
+
+        // Inicialización de las variables para almacenar la temperatura máxima y mínima
+        int temperaturaMaxima = Integer.MIN_VALUE;
+        int temperaturaMinima = Integer.MAX_VALUE;
+        String ciudadTempMaxima = "";
+        String ciudadTempMinima = "";
+
+        // Bucle para encontrar la temperatura máxima y mínima y sus respectivas ciudades
+        for (int i = 0; i < ciudades.length; i++) {
+
+            if (temperatura[i][1] > temperaturaMaxima) {
+                temperaturaMaxima = temperatura[i][1];
+                ciudadTempMaxima = ciudades[i];
+            }
+
+            if (temperatura[i][0] < temperaturaMinima) {
+                temperaturaMinima = temperatura[i][0];
+                ciudadTempMinima = ciudades[i];
+            }
+        }
+
+        // Se muestra la temperatura máxima y mínima junto con sus ciudades
+        System.out.println("La temperatura máxima registrada fue de " + temperaturaMaxima + " ºC en " + ciudadTempMaxima);
+        System.out.println("La temperatura mínima registrada fue de " + temperaturaMinima + " ºC en " + ciudadTempMinima);
+    }
+}
