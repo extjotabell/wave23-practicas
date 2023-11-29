ca686c67f4fb3fd28215a237204b93919ec0832c
Fatima Noble <fatima.noble@mercadolibre.com>
2023-11-29 18:09:36 -0300

    ejercicio de cerrera de la selva

---
+++ 2. java/ejercicia carrera de la selva/src/main/java/org/example/Participante.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Participante {
+    private  int nro,edad;
+    private  String dni,nombre,apellido,celular,grupoSanguineo;
+    public String getNombre() {
+        return nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+
+    public Participante(int nro, String dni, String nombre, String apellido, String celular, String grupoSanguineo, int edad) {
+        this.nro = nro;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.celular = celular;
+        this.grupoSanguineo = grupoSanguineo;
+        this.edad = edad;
+    }
+}
