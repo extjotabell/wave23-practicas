6663c0b19f7639e04ecdd2ba43786e15c5ef8c95
melilackingtons <martin.lackington@mercadolibre.cl>
2023-11-30 13:32:19 -0300

    Ejercicio Imc

---
+++ 2. java/ejercicio_imc/project/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Persona martin = new Persona();
+
+        Persona hernan = new Persona("Hernan",22,"123456789");
+
+        Persona zuly = new Persona("Zuly",23,"987654321",60.3,1.55);
+
+        //No sería posible crear las 2 últimas instancias porque no tenemos un constructor para ello.
+
+        int imc = zuly.calcularIMC();
+        boolean mayor = zuly.esMayorDeEdad();
+
+
+        System.out.println(zuly);
+    }
+}
