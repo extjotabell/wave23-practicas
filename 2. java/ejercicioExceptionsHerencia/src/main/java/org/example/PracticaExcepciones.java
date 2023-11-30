6ffde143f3aa91f25ca38872e237a062958c5f8a
hdesanvicente <hernan.desanvicente@mercadolibre.com>
2023-11-30 17:46:37 -0300

    Ejercicio de excepciones y herencia

---
+++ 2. java/ejercicioExceptionsHerencia/src/main/java/org/example/PracticaExcepciones.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class PracticaExcepciones {
+    private int a = 0;
+    private int b = 300;
+
+
+    public double calcularCociente() {
+
+        return this.b / this.a;
+    }
+}
