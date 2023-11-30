40572e010bf6e26f67b0e5a64a795664aee38d15
zulyvargasrz <zuly.vargasramirez@mercadolibre.com.co>
2023-11-30 15:53:29 -0500

    Refactor format response in the  solution for 'ejercicio-oop-2'

--- 2. java/ejercicio-opp-2/src/main/java/org/ejercicios/ejercicioDos/modelo/Distribuidora.java
+++ 2. java/ejercicio-opp-2/src/main/java/org/ejercicios/ejercicioDos/modelo/Distribuidora.java
@@ -9,7 +9,7 @@ public class Distribuidora {
     public static void main(String[] args) {
 
         List<Producto> productos = new ArrayList<>();
-        Producto productoUno = new Perecedero("Producto uno", 20000, 3 );
+        Producto productoUno = new Perecedero("Producto uno", 20000, 2 );
         Producto productoDos= new NoPerecedero("Producto dos", 11100, "Papeleria" );
         productos.addAll(List.of( productoUno, productoDos ));
 
@@ -18,6 +18,6 @@ public class Distribuidora {
             total += p.calcular(5);
         }
 
-        System.out.println("TOTAL VENDIDO: $" + total);
+        System.out.printf("TOTAL VENDIDO: $ %.2f" , total);
     }
 }
