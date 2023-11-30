693ead4b609522342860c52777df2c674a35e501
Sebastian Ospina Garcia <sebastian.ospina@mercadolibre.com.co>
2023-11-30 09:46:09 -0500

    (Feat): Se realizó el programa hasta el punto 6

--- 2. java/PooP1Vivo/src/main/java/org/example/Main.java
+++ 2. java/PooP1Vivo/src/main/java/org/example/Main.java
@@ -8,8 +8,32 @@ public class Main {
 
         Persona personaPrimerConstructor = new Persona("Sebas",23,"1010124016");
 
-        Persona personaSegundoConstructor = new Persona("Mariana",22,"123456789",70.0,165.0);
-        
+        Persona personaSegundoConstructor = new Persona("Mariana",22,"123456789",60.0,1.65);
+
         //Persona personaErronea = new Persona("Camilo",23);
+
+        verificarEdad(personaSegundoConstructor);
+
+        System.out.println(personaSegundoConstructor);
+
+        //Se verifica con más valores
+        personaSegundoConstructor.setEdad(17);
+        personaSegundoConstructor.setPeso(90);
+
+        verificarEdad(personaSegundoConstructor);
+
+        System.out.println(personaSegundoConstructor);
+
+
     }
+
+    public static void verificarEdad(Persona persona){
+        if (persona.esMayorDeEdad()){
+            System.out.println("La persona "+persona.getNombre()+" es mayor de edad.");
+        }
+        else {
+            System.out.println("La persona "+persona.getNombre()+" es menor de edad.");
+        }
+    }
+
 }
--- 2. java/PooP1Vivo/src/main/java/org/example/modelo/Persona.java
+++ 2. java/PooP1Vivo/src/main/java/org/example/modelo/Persona.java
@@ -2,11 +2,11 @@ package org.example.modelo;
 
 public class Persona {
 
-    String nombre;
-    int edad;
-    String dni;
-    double peso;
-    double altura;
+    private String nombre;
+    private int edad;
+    private String dni;
+    private double peso;
+    private double altura;
 
     public Persona() {
     }
@@ -24,4 +24,85 @@ public class Persona {
         this.peso = peso;
         this.altura = altura;
     }
+
+    private int calcularIMC(){
+        double imc = (this.peso)/(this.altura * this.altura);
+        if (imc < 20){
+            return -1;
+        } else if (imc >= 20 && imc <= 25) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public boolean esMayorDeEdad(){
+        return edad >= 18;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public String toString() {
+        String nivelPeso;
+        switch (this.calcularIMC()) {
+            case -1:
+                nivelPeso = "Bajo peso";
+                break;
+            case 0:
+                nivelPeso = "Peso saludable";
+                break;
+            default:
+                nivelPeso = "Sobrepeso";
+                break;
+        }
+
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", dni='" + dni + '\'' +
+                ", peso=" + peso +
+                ", altura=" + altura +
+                ", nivelPeso='" + nivelPeso + '\'' +
+                '}';
+    }
+
 }
