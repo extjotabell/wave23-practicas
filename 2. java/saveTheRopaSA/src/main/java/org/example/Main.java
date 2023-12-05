7f928284937eab7c0c1553a3315d018c3a39ea56
AlimpioDiaz <andres.limpiodiaz@mercadolibre.com.mx>
2023-12-04 19:55:32 -0600

    Add live task ~Save The Ropa S.A.~ from ~Ejercicios Integradores P2 VIVO~

---
+++ 2. java/saveTheRopaSA/src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Prenda prenda1 = new Prenda("Tommy","Polera");
+        Prenda prenda2 = new Prenda("quicksilver","pantalones");
+
+        List<Prenda> miRopa = new ArrayList<>();
+        List<Prenda> miRopa2 = new ArrayList<>();
+
+        miRopa.add(prenda1);
+        miRopa.add(prenda2);
+
+        miRopa2.add(prenda2);
+
+        GuardaRopa guardaRopa = new GuardaRopa();
+        guardaRopa.guardarPrendas(miRopa);
+        guardaRopa.guardarPrendas(miRopa2);
+
+        //GuardaRopa guardaRopa1 = new GuardaRopa();
+
+        //guardaRopa1.guardarPrendas(miRopa2);
+
+        //guardaRopa1.mostrarPrendas();
+
+        guardaRopa.mostrarPrendas();
+        guardaRopa.devolverPrendas(1);
+        guardaRopa.mostrarPrendas();
+    }
+}
