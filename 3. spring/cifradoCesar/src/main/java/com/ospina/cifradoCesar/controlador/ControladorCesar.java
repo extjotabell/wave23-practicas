782693251f432ab7ad93f02e2de801b488393105
Sebastian Ospina Garcia <sebastian.ospina@mercadolibre.com.co>
2023-12-05 15:41:26 -0500

    (Feat): Se agregó manejo de excepciones

--- 3. spring/cifradoCesar/src/main/java/com/ospina/cifradoCesar/controlador/ControladorCesar.java
+++ 3. spring/cifradoCesar/src/main/java/com/ospina/cifradoCesar/controlador/ControladorCesar.java
@@ -1,28 +1,32 @@
 package com.ospina.cifradoCesar.controlador;
 
 import com.ospina.cifradoCesar.modelo.MensajeRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/cesar")
-
 public class ControladorCesar {
+
     private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    @GetMapping("/encriptar")
-    public String encriptar(@RequestBody MensajeRequest mensajeRequest) {
-        String mensaje = mensajeRequest.getMensaje().toUpperCase().replaceAll("\\s", "");
-        String clave23 = "23";
-        String clave012 = "012";
+    @PostMapping("/encriptar")
+    public ResponseEntity<String> encriptar(@RequestBody MensajeRequest mensajeRequest) {
+        try {
+            String mensaje = mensajeRequest.getMensaje().toUpperCase().replaceAll("\\s", "");
+            String clave23 = "23";
+            String clave012 = "012";
 
-        String mensajeEncriptadoClave23 = encriptarMensaje(mensaje, clave23);
-        String mensajeEncriptadoClave012 = encriptarMensaje(mensaje, clave012);
+            String mensajeEncriptadoClave23 = encriptarMensaje(mensaje, clave23);
+            String mensajeEncriptadoClave012 = encriptarMensaje(mensaje, clave012);
 
-        return "Mensaje encriptado (clave 23): " + mensajeEncriptadoClave23 + "\n" +
-                "Mensaje encriptado (clave 012): " + mensajeEncriptadoClave012;
+            return ResponseEntity.ok("Mensaje encriptado (clave 23): " + mensajeEncriptadoClave23 + "\n" +
+                    "Mensaje encriptado (clave 012): " + mensajeEncriptadoClave012);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al encriptar el mensaje: " + e.getMessage());
+        }
     }
 
     private String encriptarMensaje(String mensaje, String clave) {
@@ -30,6 +34,9 @@ public class ControladorCesar {
         int indiceClave = 0;
         for (int i = 0; i < mensaje.length(); i++) {
             int indiceLetra = ALPHABET.indexOf(mensaje.charAt(i));
+            if (indiceLetra == -1) {
+                throw new IllegalArgumentException("Caracter no permitido en el mensaje: " + mensaje.charAt(i));
+            }
             int indiceClaveActual = Integer.parseInt(String.valueOf(clave.charAt(indiceClave)));
             int indiceLetraEncriptada = (indiceLetra + indiceClaveActual) % ALPHABET.length();
             mensajeEncriptado.append(ALPHABET.charAt(indiceLetraEncriptada));
@@ -38,4 +45,9 @@ public class ControladorCesar {
         return mensajeEncriptado.toString();
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Error: " + ex.getMessage());
+    }
 }
