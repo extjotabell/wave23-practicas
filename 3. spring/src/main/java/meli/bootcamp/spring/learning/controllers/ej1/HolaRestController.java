e148fd16c6de80c31470c5e8c4df1d2b38245b17
Tiago Alberto Ramirez Marchisio <tiago.ramirezmarchisio@mercadolibre.com>
2023-12-05 12:58:15 -0300

    Terminado ejercicio 2

--- 3. spring/src/main/java/meli/bootcamp/spring/learning/controllers/ej1/HolaRestController.java
+++ 3. spring/src/main/java/meli/bootcamp/spring/learning/controllers/ej1/HolaRestController.java
@@ -1,10 +1,12 @@
-package meli.bootcamp.spring.learning.controllers;
+package meli.bootcamp.spring.learning.controllers.ej1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
+@RequestMapping("ej1")
 public class HolaRestController {
   @GetMapping
   public String sayHello() {
