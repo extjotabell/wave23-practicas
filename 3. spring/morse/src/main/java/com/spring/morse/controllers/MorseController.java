6d345b9dae3a18cf6e3e312f6ef7d6e7a229f835
camilamamanill <camila.mamanillanquichoque@mercadolibre.com>
2023-12-06 12:07:16 -0300

    changed pathvariable to requestparam

--- 3. spring/morse/src/main/java/com/spring/morse/controllers/MorseController.java
+++ 3. spring/morse/src/main/java/com/spring/morse/controllers/MorseController.java
@@ -3,18 +3,19 @@ package com.spring.morse.controllers;
 import com.spring.morse.entities.MorseHelper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MorseController {
 
     @GetMapping("/descifrar/{morseMensaje}")
-    public String descifrarMorse(@PathVariable String morseMensaje) {
+    public String descifrarMorse(@RequestParam String morseMensaje) {
         return MorseHelper.getSentenceFromMorseSentence(morseMensaje);
     }
 
-    @GetMapping("/cifrar/{mensaje}")
-    public String cifrarMensaje(@PathVariable String mensaje) {
+    @GetMapping("/cifrar/")
+    public String cifrarMensaje(@RequestParam String mensaje) {
         return MorseHelper.getMorseSentenceFromABCSentence(mensaje);
     }
 
