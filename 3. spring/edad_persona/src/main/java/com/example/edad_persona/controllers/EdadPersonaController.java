ce99cd65a093442302e97d43c48643b790cb09bc
miguelOchoaM <miguel.ochoa@mercadolibre.com.co>
2023-12-07 13:58:43 -0500

    Agregando el ejercicio de crear una API para calcular la edad de las personas

---
+++ 3. spring/edad_persona/src/main/java/com/example/edad_persona/controllers/EdadPersonaController.java
@@ -0,0 +1,34 @@
+package com.example.edad_persona.controllers;
+
+import com.example.edad_persona.dto.Persona;
+import com.example.edad_persona.services.EdadPersonaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class EdadPersonaController {
+
+    private final EdadPersonaService edadPersonaService;
+
+    public EdadPersonaController(EdadPersonaService edadPersonaService) {
+        this.edadPersonaService = edadPersonaService;
+    }
+
+    @GetMapping("/{day}/{month}/{year}")
+    public int calcularEdad(@PathVariable int day, @PathVariable int month, @PathVariable int year) {
+        return edadPersonaService.calcularEdad(day, month, year);
+    }
+
+    @PostMapping("/{day}/{month}/{year}")
+    public Persona agregarPersona(@PathVariable int day, @PathVariable int month, @PathVariable int year, @RequestBody Persona persona) {
+        edadPersonaService.agregarPersona(persona, day, month, year);
+        return persona;
+    }
+
+    @GetMapping("/listAll")
+    public List<Persona> listAll() {
+        return edadPersonaService.getPersonas();
+    }
+}
---
+++ 3. spring/edad_persona/src/main/java/com/example/edad_persona/services/EdadPersonaService.java
@@ -0,0 +1,30 @@
+package com.example.edad_persona.services;
+
+import com.example.edad_persona.dto.Persona;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EdadPersonaService {
+
+    private List<Persona> personas = new ArrayList<>();
+
+    public int calcularEdad(int day, int month, int year) {
+        LocalDate now = LocalDate.now();
+
+        return now.getMonthValue() > month || now.getMonthValue() == month && now.getDayOfMonth() >= day ?
+                (now.getYear() - year) : (now.getYear() - year - 1);
+    }
+
+    public void agregarPersona(Persona p, int day, int month, int year) {
+        p.setEdad(calcularEdad(day, month, year));
+        personas.add(p);
+    }
+
+    public List<Persona> getPersonas() {
+        return personas;
+    }
+}
