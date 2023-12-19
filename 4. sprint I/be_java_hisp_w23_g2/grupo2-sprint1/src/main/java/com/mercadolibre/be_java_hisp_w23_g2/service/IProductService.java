96d4d137bcc7486fbbcc17bf442118a96f4081e6
melilackingtons <martin.lackington@mercadolibre.cl>
2023-12-19 16:26:30 -0300

    Delete 4. sprint I/be_java_hisp_w23_g2/grupo2-sprint1 directory

--- 4. sprint I/be_java_hisp_w23_g2/grupo2-sprint1/src/main/java/com/mercadolibre/be_java_hisp_w23_g2/service/IProductService.java
+++
@@ -1,8 +0,0 @@
-package com.mercadolibre.be_java_hisp_w23_g2.service;
-
-import com.mercadolibre.be_java_hisp_w23_g2.dto.MessageDTO;
-import com.mercadolibre.be_java_hisp_w23_g2.dto.PostDTO;
-
-public interface IProductService {
-    MessageDTO addPost(PostDTO postDTO);
-}
