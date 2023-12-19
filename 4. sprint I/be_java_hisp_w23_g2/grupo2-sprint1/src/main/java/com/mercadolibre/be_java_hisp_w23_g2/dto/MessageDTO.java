367bdf235a768269191834096b2640746fbbc361
melilackingtons <martin.lackington@mercadolibre.cl>
2023-12-19 11:22:08 -0300

    Group Part

---
+++ 4. sprint I/be_java_hisp_w23_g2/grupo2-sprint1/src/main/java/com/mercadolibre/be_java_hisp_w23_g2/dto/MessageDTO.java
@@ -0,0 +1,12 @@
+package com.mercadolibre.be_java_hisp_w23_g2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDTO {
+    private String message;
+}
