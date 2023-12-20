84f7b04df1e86b5dcf2f0a576bdb2f12f70edd5d
Fatima Noble <fatima.noble@mercadolibre.com>
2023-12-20 11:52:19 -0300

    eliminar carpeta de sprint 1 grupal desactualizada

--- 4. sprint I/be_java_hisp_w23_g05/src/main/java/meli/bootcamp/sprint1/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package meli.bootcamp.sprint1.exception;
-
-public class BadRequestException extends RuntimeException {
-  public BadRequestException(String message) {
-    super(message);
-  }
-}
--- 4. sprint I/be_java_hisp_w23_g05/src/main/java/meli/bootcamp/sprint1/exception/EmptyListException.java
+++
@@ -1,9 +0,0 @@
-package meli.bootcamp.sprint1.exception;
-
-public class EmptyListException extends RuntimeException{
-
-    public EmptyListException(String message){
-        super(message);
-    }
-
-}
