6fe4ec19af780a6d01e74f93f0b1df388fcbd8be
melilackingtons <martin.lackington@mercadolibre.cl>
2023-12-20 13:22:44 -0300

    Parte Grupal

---
+++ 4. sprint I/be_java_hisp_w23_g2/grupo2-sprint1/src/main/java/com/mercadolibre/be_java_hisp_w23_g2/exception/NotFollowingException.java
@@ -0,0 +1,10 @@
+package com.mercadolibre.be_java_hisp_w23_g2.exception;
+
+public class NotFollowingException extends RuntimeException{
+    public NotFollowingException(String message){
+        super(message);
+    }
+
+    public NotFollowingException() {
+    }
+}
