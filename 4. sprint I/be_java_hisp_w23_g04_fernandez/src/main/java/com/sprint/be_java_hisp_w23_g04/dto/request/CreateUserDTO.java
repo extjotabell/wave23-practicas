9f5d43134afbaa241519623821f012fe995b081e
CinferduML <cinthya.fernandez@mercadolibre.com>
2023-12-20 16:06:05 -0300

    Postman collection

--- 4. sprint I/be_java_hisp_w23_g04_fernandez/src/main/java/com/sprint/be_java_hisp_w23_g04/dto/request/CreateUserDTO.java
+++ 4. sprint I/be_java_hisp_w23_g04_fernandez/src/main/java/com/sprint/be_java_hisp_w23_g04/dto/request/CreateUserDTO.java
@@ -1,8 +1,6 @@
 package com.sprint.be_java_hisp_w23_g04.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.sprint.be_java_hisp_w23_g04.dto.response.PostResponseDTO;
-import com.sprint.be_java_hisp_w23_g04.dto.response.UserFollowDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
