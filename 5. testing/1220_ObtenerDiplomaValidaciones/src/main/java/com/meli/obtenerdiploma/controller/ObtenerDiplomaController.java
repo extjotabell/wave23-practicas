c3b1996c1b38c1748865f83077f27d02b84123ad
Tonatiuh Díaz <alfredo.diazgomez@mercadolibre.com.mx>
2023-12-21 17:22:48 -0600

    Se agrega ejercicio de validaciones

---
+++ 5. testing/1220_ObtenerDiplomaValidaciones/src/main/java/com/meli/obtenerdiploma/controller/ObtenerDiplomaController.java
@@ -0,0 +1,32 @@
+package com.meli.obtenerdiploma.controller;
+
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.service.IObtenerDiplomaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Objects;
+
+@RestController
+public class ObtenerDiplomaController {
+
+    @Autowired
+    IObtenerDiplomaService service;
+
+    @PostMapping("/analyzeScores")
+    public StudentDTO analyzeScores(@RequestBody @Valid StudentDTO rq) {
+        return service.analyzeScores(rq);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<HashMap<?, ?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        HashMap<String, Object> response = new HashMap<>();
+        response.put("message", e.getBindingResult().getFieldErrors().get(0).getDefaultMessage());
+        return ResponseEntity.badRequest().body(response);
+    }
+
+}
