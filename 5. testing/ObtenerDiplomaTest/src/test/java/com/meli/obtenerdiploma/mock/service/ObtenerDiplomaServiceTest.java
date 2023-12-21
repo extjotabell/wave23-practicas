586a8d9000573982c965740b9aea737464d593e6
ferMezaMel <fernando.mezaortega@mercadolibre.com.mx>
2023-12-21 14:12:36 -0600

    Add files for test exercise and modify some

--- 5. testing/ObtenerDiplomaTest/src/test/java/com/meli/obtenerdiploma/mock/service/ObtenerDiplomaServiceTest.java
+++ 5. testing/ObtenerDiplomaTest/src/test/java/com/meli/obtenerdiploma/mock/service/ObtenerDiplomaServiceTest.java
@@ -1,2 +1,51 @@
-package com.meli.obtenerdiploma.mock.service;public class ObtenerDiplomaServiceTest {
+package com.meli.obtenerdiploma.mock.service;
+
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.model.SubjectDTO;
+import com.meli.obtenerdiploma.repository.IStudentDAO;
+import com.meli.obtenerdiploma.service.ObtenerDiplomaService;
+import org.apache.catalina.filters.RequestDumperFilter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ObtenerDiplomaServiceTest {
+    @Mock
+    private IStudentDAO studentDAO;
+
+    @InjectMocks
+    private ObtenerDiplomaService obtenerDiplomaService;
+
+    @Test
+    @DisplayName("Validación de servicio obtener diploma en método analyzeScores")
+    void analyzeScoresTest(){
+
+    }
+
+    @Test
+    @DisplayName("Validación de cálculo de promedio y mensaje")
+    void calculateAverageTest(){
+        StudentDTO studentWhen = new StudentDTO();
+        when(studentDAO.findById(anyLong())).thenReturn(studentWhen);
+        StudentDTO expected = new StudentDTO();
+        System.out.println(studentWhen.toString());
+        Assertions.assertEquals(expected, studentWhen);
+    }
+
+    private StudentDTO createStudent() {
+        return new StudentDTO();
+    }
+
+
 }
--- 5. testing/ObtenerDiplomaTest/src/test/java/com/meli/obtenerdiploma/repositoryTest/StudentRepositoryTest.java
+++ 5. testing/ObtenerDiplomaTest/src/test/java/com/meli/obtenerdiploma/repositoryTest/StudentRepositoryTest.java
@@ -1,2 +1,32 @@
-package com.meli.obtenerdiploma.repositoryTest;public class StudentRepositoryTest {
+package com.meli.obtenerdiploma.repositoryTest;
+
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.repository.IStudentRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.Set;
+
+@SpringBootTest
+public class StudentRepositoryTest {
+
+    @Autowired
+    private IStudentRepository studentRepository;
+    @Test
+    @DisplayName("Verificar que el conjunto de estudiante no sea nulo")
+    void findAllNullTest(){
+        Assertions.assertNotNull(studentRepository.findAll());
+    }
+
+    @Test
+    @DisplayName("Verificar que el conjunto de estudiantes no sea vacío")
+    void findAllNotEmptyTest() {
+        int studentsSize = studentRepository.findAll().size();
+        int notExpectedSize = 0;
+        Assertions.assertNotEquals(notExpectedSize, studentsSize);
+    }
 }
