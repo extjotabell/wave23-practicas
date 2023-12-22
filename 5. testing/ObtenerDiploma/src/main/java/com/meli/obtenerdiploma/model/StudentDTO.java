4d4ce01ba3ec8c72fa32e6d53f4e63cb302bc30e
miguelOchoaM <miguel.ochoa@mercadolibre.com.co>
2023-12-22 15:14:32 -0500

    Agregando el ejercicio de obtener diploma (Validaciones)

---
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -0,0 +1,26 @@
+package com.meli.obtenerdiploma.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.util.List;
+
+@Getter @Setter
+public class StudentDTO {
+
+    @NotNull
+    private Long id;
+
+    @NotBlank(message = "El nombre del estudiante no puede estar vacío.")
+    @Pattern(regexp="([A-Z]|[0-9])[\\s|[0-9]|A-Z|a-z|ñ|ó|í|á|é|ú|Á|Ó|É|Í|Ú]*$", message = "El nombre del estudiante debe comenzar con mayúscula.")
+    @Size(max = 50, message = "La longitud del nombre del estudiante no puede superar los 50 caracteres.")
+    private String studentName;
+
+    private String message;
+    private Double averageScore;
+
+    @NotEmpty(message = "La lista de materias no puede estar vacía.")
+    private List<@Valid SubjectDTO> subjects;
+}
