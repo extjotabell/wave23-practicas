b27adc4110fecf7a286ad5e885c2bf41e5226d24
zulyvargasrz <zuly.vargasramirez@mercadolibre.com.co>
2023-12-22 15:48:36 -0500

    Add Repository Find All test

---
+++ 5. testing/ObtenerDiploma-mocks/src/test/java/com/meli/obtenerdiploma/repository/StudentRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.meli.obtenerdiploma.repository;
+
+import com.meli.obtenerdiploma.model.StudentDTO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@SpringBootTest
+class StudentRepositoryTest {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Test
+    @DisplayName("Encontrar todos los alumnos")
+    void testFindAll(){
+        //Arrange
+        Set<StudentDTO> result = new HashSet<>();
+        //Act
+        result = studentRepository.findAll();
+        // Assert
+        assertNotNull(result);
+    }
+}
