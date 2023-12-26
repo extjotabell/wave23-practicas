2b908b1ed3b4159d6a6d63c19ca476977b99ffca
araaquino <arami.aquino@mercadolibre.com>
2023-12-26 12:48:07 -0300

    Refactoring Integration Test starWars

--- 5. testing/starwars/src/test/java/com/mercadolibre/starwars/IntegrationTest/IntegrationTest.java
+++ 5. testing/starwars/src/test/java/com/mercadolibre/starwars/IntegrationTest/IntegrationTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.mercadolibre.starwars.dto.CharacterDTO;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,10 +29,37 @@ public class IntegrationTest {
     @Autowired
     MockMvc mockMvc;
 
+    // ObjectWriter to convert objects to JSON with specific serialization settings
+    private final ObjectWriter objecTWriter = new ObjectMapper()
+            .configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer();
+    
     @Test
-    void getCharacterOKTestGet() throws Exception{
+    @DisplayName("Test method for retrieving a character")
+    void getCharacterOKTestGet() throws Exception {
+        // Create a list containing a new CharacterDTO for Luke Skywalker
+        List<CharacterDTO> responseDTO = createNewCharacterLukeDTO();
 
-        //como devuelve un characterDTO, preparamos:
+        // Convert the response DTO to JSON
+        String responseJson = this.objecTWriter.writeValueAsString(responseDTO);
+
+        // Perform a GET request to retrieve a character and expect an OK response
+        MvcResult mvcResult = this.mockMvc.perform(get("/{query}", "Luke Skywalker"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andReturn();
+
+        // Verify that the correct response JSON is returned
+        assertEquals(responseJson, mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8));
+    }
+
+    /**
+     * Private method to create a list containing a new CharacterDTO for Luke Skywalker.
+     *
+     * @return a list containing a new CharacterDTO for Luke Skywalker
+     */
+    private List<CharacterDTO> createNewCharacterLukeDTO() {
+        // Create a new CharacterDTO for Luke Skywalker
         CharacterDTO characterDTO = new CharacterDTO(
                 "Luke Skywalker",
                 "blond",
@@ -44,23 +73,10 @@ public class IntegrationTest {
                 77
         );
 
+        // Create a list containing the new CharacterDTO
         List<CharacterDTO> responseDTO = new ArrayList<>();
         responseDTO.add(characterDTO);
 
-        //Segundo : deserializer to string - JSON manualmente
-        ObjectWriter objecTWriter = new ObjectMapper()
-                .configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer();
-        //dato de salida
-        String responseJson = objecTWriter.writeValueAsString(responseDTO);
-
-        //when
-        MvcResult mvcResult = this.mockMvc.perform(get("/{query}", "Luke Skywalker"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-                .andReturn();
-
-        assertEquals(responseJson, mvcResult.getResponse().getContentAsString());
-
+        return responseDTO;
     }
 }
