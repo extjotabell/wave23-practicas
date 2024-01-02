bc019e958b0f95042951372c2ef7c617f7a291b8
AlimpioDiaz <andres.limpiodiaz@mercadolibre.com.mx>
2024-01-02 13:56:00 -0600

    Add US0007 integration test to individual part

--- 6. sprint II/be_java_hisp_w23_g01_limpio/.idea/sonarlint/securityhotspotstore/index.pb
+++ 6. sprint II/be_java_hisp_w23_g01_limpio/.idea/sonarlint/securityhotspotstore/index.pb
@@ -5,3 +5,5 @@ u
 Esrc/test/java/com/meli/socialmeli/controllers/UserControllerTest.java,c/b/cb4763ea01417148e4a79feeba06547053a5502b
 Å
 Qsrc/test/java/com/meli/socialmeli/integrations/UserControllerIntegrationTest.java,8/3/834bb98786c6faadf496124d3044361a3b17dabf
+q
+Asrc/main/java/com/meli/socialmeli/controllers/UserController.java,5/6/5668cad2ff0eaa3f4aa84388cc566121ede1537b
--- 6. sprint II/be_java_hisp_w23_g01_limpio/src/test/java/com/meli/socialmeli/integrations/UserControllerIntegrationTest.java
+++ 6. sprint II/be_java_hisp_w23_g01_limpio/src/test/java/com/meli/socialmeli/integrations/UserControllerIntegrationTest.java
@@ -7,14 +7,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class UserControllerIntegrationTest {
+class UserControllerIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
@@ -33,6 +35,30 @@ public class UserControllerIntegrationTest {
     @DisplayName("Test para verificar el camino malo del endpoint GetALLUsers (US-0001)")
     void testGetAllUsersEndpointBadWay() throws Exception {
         this.mockMvc.perform(get("/user")
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isNotFound())
+                    .andReturn().getResponse().getContentAsString();
+    }
+
+    @Test
+    @DisplayName("Test para verificar el camino bueno del endpoint unfollowUser (US-0007)")
+    void testUnfollowUserEndpoint() throws Exception {
+        //Creacion de un seguidor
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/3100/follow/100")
+                    .contentType(MediaType.APPLICATION_JSON));
+
+        this.mockMvc.perform(post("/users/3100/unfollow/100")
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andReturn().getResponse().getContentAsString();
+    }
+    @Test
+    @DisplayName("Test para verificar el camino bueno del endpoint unfollowUser (US-0007)")
+    void testUnfollowUserEndpointBadWay() throws Exception {
+
+        this.mockMvc.perform(post("/users/31030/unfollow/100")
                     .contentType(MediaType.APPLICATION_JSON))
                     .andDo(print())
                     .andExpect(status().isNotFound())
