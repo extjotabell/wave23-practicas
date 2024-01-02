27979576c8488d5db20dc76748d114d3a3ee500a
zulyvargasrz <zuly.vargasramirez@mercadolibre.com.co>
2024-01-02 12:44:05 -0500

    Add Integration test: POST  create product return confirmation

---
+++ 6. sprint II/be_java_hisp_w23_g01_vargas/src/test/java/com/meli/socialmeli/integration/ProductControllerIntegrationTest.java
@@ -0,0 +1,52 @@
+package com.meli.socialmeli.integration;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.meli.socialmeli.dtos.request.PostDTO;
+import com.meli.socialmeli.dtos.response.ProductDTO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    private static ObjectWriter writer = new ObjectMapper()
+            .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+            .registerModule(new JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .writer().withDefaultPrettyPrinter();
+
+    @Test
+    @DisplayName("POST: /products - Return creation confirmation")
+    void createProductReturnConfirmation() throws Exception {
+        //Arrange
+        ProductDTO productDTO = new ProductDTO(111, "Camisa", "Camisa polo", "Polo", "blue", "algodon puro");
+        PostDTO postDTO = new PostDTO(100, LocalDate.now(), productDTO, 1, 100000.0);
+
+        String responseJSONExpected = "{'message': 'The User 100 has created new post.'}";
+        //Act - Assert
+        mockMvc.perform(post("/products/post")
+                        .contentType("application/json")
+                        .content(writer.writeValueAsString(postDTO))
+                        .accept("application/json"))
+                .andExpect(content().json(responseJSONExpected))
+                .andExpect(status().isOk());
+    }
+}
