7db5f9c7b01abafec30277b8d5b09a90c520bb6c
ferMezaMel <fernando.mezaortega@mercadolibre.com.mx>
2024-01-02 14:33:45 -0600

    Add base files for individual and extra parts of sprint 2

---
+++ 6. sprint II/be_java_hisp_w23_g01_meza/src/test/java/com/meli/socialmeli/integration/UserIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.meli.socialmeli.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+    private static ObjectWriter objectWriter;
+
+    @BeforeAll
+    public static void setUp() {
+        objectWriter = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer()
+                .withDefaultPrettyPrinter();
+    }
+
+
+}
