2dea54a7bfb65b84fa1791440486307b60003376
lucassalerno <lucas.salerno@mercadolibre.com>
2024-01-02 13:41:34 -0300

    Individual Sprint 2 - Integration Test: GetLastPost + Refactoring in Previous Code.

--- 6. sprint II/repoSprint2/src/main/java/meli/bootcamp/sprint1/entity/Post.java
+++ 6. sprint II/repoSprint2/src/main/java/meli/bootcamp/sprint1/entity/Post.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
--- 6. sprint II/repoSprint2/src/test/java/meli/bootcamp/sprint1/integration/IntegrationTestUserController.java
+++ 6. sprint II/repoSprint2/src/test/java/meli/bootcamp/sprint1/integration/IntegrationTestUserController.java
@@ -3,12 +3,8 @@ package meli.bootcamp.sprint1.integration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import meli.bootcamp.sprint1.dto.response.BaseResponseDto;
-import meli.bootcamp.sprint1.dto.response.FollowersDto;
-import meli.bootcamp.sprint1.dto.response.UserWFollowerListDto;
-import meli.bootcamp.sprint1.entity.User;
-import meli.bootcamp.sprint1.repository.impl.GeneralRepository;
-import org.junit.jupiter.api.BeforeEach;
+import meli.bootcamp.sprint1.dto.response.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,14 +12,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static meli.bootcamp.sprint1.utils.Factory.generateUserDtoList;
+import static meli.bootcamp.sprint1.utils.Factory.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -32,8 +28,6 @@ public class IntegrationTestUserController {
     @Autowired
     private MockMvc mockMvc;
 
-    private GeneralRepository repository = new GeneralRepository();
-
     private ObjectWriter objectWriter = new ObjectMapper()
             .registerModule(new JavaTimeModule())
             .writer();
@@ -41,43 +35,79 @@ public class IntegrationTestUserController {
     private FollowersDto followers;
     private BaseResponseDto responseDto;
     private UserWFollowerListDto user = generateUserDtoList("name_asc");
+    private LastPostsDto lastPostsDto;
 
     @Test
+    @DisplayName("Endpoint - GetFollowers -> OK")
     void testGetFollowers_Ok() throws Exception{
         followers = new FollowersDto(user.getUser_id(), user.getUser_name(), user.getFollowers().size());
 
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/users/{userId}/followers/count", 4);
 
-        ResultMatcher statusExpected = MockMvcResultMatchers.status().isOk();
-        ResultMatcher contentTypeExpected = MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON);
-
         MvcResult result = mockMvc.perform(request)
                 .andDo(print())
-                .andExpect(statusExpected)
-                .andExpect(contentTypeExpected)
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andReturn();
 
-        assertEquals(result.getResponse().getContentAsString(), objectWriter.writeValueAsString(followers));
+        assertEquals(objectWriter.writeValueAsString(followers), result.getResponse().getContentAsString());
     }
 
     @Test
+    @DisplayName("Endpoint - GetFollowers -> Exception")
     void testGetFollowers_ThrowsBadRequestException() throws Exception{
         responseDto = new BaseResponseDto("User not found");
 
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/users/{userId}/followers/count", 999);
 
-        ResultMatcher statusExpected = MockMvcResultMatchers.status()
-                .isBadRequest();
-        ResultMatcher contentTypeExpected = MockMvcResultMatchers.content()
-                .contentType(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        assertEquals(objectWriter.writeValueAsString(responseDto), result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @DisplayName("Endpoint - LastPost -> OK")
+    void testGetLastPost_WithOrder_Ok() throws Exception{
+        String order = "date_asc";
+        lastPostsDto = generateLastPostDtoOrdered(order);
+        /*
+        List<PostDto> posts = new ArrayList<>(lastPostsDto.getPosts());
+        posts.stream()
+                .forEach(postDto -> service.addPost(objectMapper.convertValue(postDto, NewPostDto.class)));
+        */
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/products/followed/{userId}/list", 2)
+                .param("order", order);
 
         MvcResult result = mockMvc.perform(request)
                 .andDo(print())
-                .andExpect(statusExpected)
-                .andExpect(contentTypeExpected)
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andReturn();
 
-        assertEquals(result.getResponse().getContentAsString(), objectWriter.writeValueAsString(responseDto));
+        assertEquals(objectWriter.writeValueAsString(lastPostsDto), result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @DisplayName("Endpoint - LastPost -> Exception")
+    void testGetLastPost_WithOrder_ThrowsBadRequestException() throws Exception{
+        String order = "wrong_param";
+        responseDto = new BaseResponseDto("Parameter '" + order + "' is not valid");
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/products/followed/{userId}/list", 2)
+                .param("order", order);
+
+        MvcResult result = mockMvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        assertEquals(objectWriter.writeValueAsString(responseDto), result.getResponse().getContentAsString());
     }
 
 }
