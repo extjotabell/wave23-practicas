03d0a7fd7af89b993ecd13b974d14e8546814da0
Tiago Alberto Ramirez Marchisio <tiago.ramirezmarchisio@mercadolibre.com>
2024-01-02 13:02:26 -0300

    Added integration test to us0003 and us0004

--- 6. sprint II/be_java_hisp_w23_g5_ramirez/src/test/java/meli/bootcamp/sprint1/integration/UserControllerIntegrationTest.java
+++ 6. sprint II/be_java_hisp_w23_g5_ramirez/src/test/java/meli/bootcamp/sprint1/integration/UserControllerIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.hamcrest.Matchers.hasSize;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,7 +20,7 @@ public class UserControllerIntegrationTest {
   private MockMvc mockMvc;
 
   @Test
-  void calculateWorking() throws Exception {
+  void us0002() throws Exception {
 
     // Request
     MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/users/{userId}/followers/count", 7);
@@ -36,6 +37,50 @@ public class UserControllerIntegrationTest {
         .andExpect(status) // expected status
         .andExpect(contentType) // expected contentType
         .andExpect(bodyPart) // expected body
+        .andDo(MockMvcResultHandlers.print()); // print req and resp
+  }
+
+  @Test
+  void us0003() throws Exception {
+    // Request
+    MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/users/{userId}/followers/list", 7);
+    // Status
+    ResultMatcher status = MockMvcResultMatchers.status().isOk();
+
+    // Content Type
+    ResultMatcher contentType = MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON);
+
+    // Body Part
+    ResultMatcher bodyPart1 = MockMvcResultMatchers.jsonPath("$.followers").value(hasSize(1));
+    ResultMatcher bodyPart2 = MockMvcResultMatchers.jsonPath("$.followers[0].user_name").value("Lucas Salerno");
+
+    mockMvc.perform(request) // request
+        .andExpect(status) // expected status
+        .andExpect(contentType) // expected contentType
+        .andExpect(bodyPart1) // expected body 1
+        .andExpect(bodyPart2) // expected body 2
+        .andDo(MockMvcResultHandlers.print()); // print req and resp
+  }
+
+  @Test
+  void us0004() throws Exception {
+    // Request
+    MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/users/{userId}/followed/list", 1);
+    // Status
+    ResultMatcher status = MockMvcResultMatchers.status().isOk();
+
+    // Content Type
+    ResultMatcher contentType = MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON);
+
+    // Body Part
+    ResultMatcher bodyPart1 = MockMvcResultMatchers.jsonPath("$.followed").value(hasSize(3));
+    ResultMatcher bodyPart2 = MockMvcResultMatchers.jsonPath("$.followed[1].user_name").value("Fatima Noble");
+
+    mockMvc.perform(request) // request
+        .andExpect(status) // expected status
+        .andExpect(contentType) // expected contentType
+        .andExpect(bodyPart1) // expected body 1
+        .andExpect(bodyPart2) // expected body 2
         .andDo(MockMvcResultHandlers.print()); // print req and resp
   }
 }
