d465590bab0a9ae6ce976afba8ae4a708b8b2fb8
AlimpioDiaz <andres.limpiodiaz@mercadolibre.com.mx>
2024-01-02 12:01:40 -0600

    Add group project Sprint II test structure files

---
+++ 6. sprint II/be_java_hisp_w23_g01/src/test/java/com/meli/socialmeli/SocialmeliApplicationTests.java
@@ -0,0 +1,14 @@
+package com.meli.socialmeli;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmeliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
