9bd5d3da869c9ddd4cc574572bdedf75382e0f6c
mcostanzo31 <marcos.costanzo@mercadolibre.com>
2024-01-03 12:35:04 -0300

    Agrego test de integracion 4

--- 6. sprint II/be_java_hisp_w23_g02_costanzo/src/test/java/com/mercadolibre/be_java_hisp_w23_g2/integration/UserControllerIntegrationTest.java
+++ 6. sprint II/be_java_hisp_w23_g02_costanzo/src/test/java/com/mercadolibre/be_java_hisp_w23_g2/integration/UserControllerIntegrationTest.java
@@ -4,17 +4,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.mercadolibre.be_java_hisp_w23_g2.dto.ProductBasicDTO;
+import com.mercadolibre.be_java_hisp_w23_g2.dto.UserBasicDTO;
 import com.mercadolibre.be_java_hisp_w23_g2.dto.requests.PostDTO;
 import com.mercadolibre.be_java_hisp_w23_g2.dto.responses.MessageDTO;
+import com.mercadolibre.be_java_hisp_w23_g2.dto.responses.UserFollowedDTO;
 import com.mercadolibre.be_java_hisp_w23_g2.dto.responses.UserFollowersCountDTO;
+import com.mercadolibre.be_java_hisp_w23_g2.entity.User;
+import com.mercadolibre.be_java_hisp_w23_g2.utils.ObjectCreator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -24,13 +30,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class UserControllerIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
 
     @Test
-    @DisplayName("Test verifying the addition of a new post")
+    @DisplayName("TI-0001: Test verifying the addition of a new post")
     void addPostIntegrationTest() throws Exception {
         PostDTO postIn = new PostDTO(2, LocalDate.now().minusWeeks(1), new ProductBasicDTO(1, "MacBook Pro", "Electronics", "Apple", "Silver", "Powerful laptop for professionals"), 1, 500.0);
         MessageDTO postOut = new MessageDTO("Publication successfully added.");
@@ -52,7 +59,7 @@ class UserControllerIntegrationTest {
     }
 
     @Test
-    @DisplayName("Test that verifies the launching of the exception for creating a post that already exists")
+    @DisplayName("TI-0002: Test that verifies the launching of the exception for creating a post that already exists")
     void addPostIntegrationExistPostException2Test() throws Exception {
         PostDTO postIn = new PostDTO(2, LocalDate.now().minusWeeks(1), new ProductBasicDTO(1, "MacBook Pro", "Electronics", "Apple", "Silver", "Powerful laptop for professionals"), 1, 500.0);
         MessageDTO postOut = new MessageDTO("The product id already exists.");
@@ -79,7 +86,7 @@ class UserControllerIntegrationTest {
     }
 
     @Test
-    @DisplayName("Test that verifying the count of followers for a specific user")
+    @DisplayName("TI-0003: Test that verifies the count of followers for a specific user")
     void getFollowersCountIntegrationTest() throws Exception {
         UserFollowersCountDTO expected = new UserFollowersCountDTO(1, "John Doe", 2);
 
@@ -90,6 +97,24 @@ class UserControllerIntegrationTest {
         String expectedJson = writer.writeValueAsString(expected);
 
         this.mockMvc.perform(get("/users/{userId}/followers/count", 1))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(content().json(expectedJson));
+    }
+
+    @Test
+    @DisplayName("TI-0004: Test that verifies the functionality of following user")
+    void followUserIntegrationTest() throws Exception {
+        UserFollowedDTO expected = new UserFollowedDTO(2, "Alice Smith", List.of(new UserBasicDTO(1, "John Doe"), new UserBasicDTO(4, "Eve Wilson"), new UserBasicDTO(5, "Charlie Brown")));
+
+        ObjectMapper writer = new ObjectMapper();
+        writer.registerModule(new JavaTimeModule());
+        writer.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false).writer();
+
+        String expectedJson = writer.writeValueAsString(expected);
+
+        this.mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}", 2, 1))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
