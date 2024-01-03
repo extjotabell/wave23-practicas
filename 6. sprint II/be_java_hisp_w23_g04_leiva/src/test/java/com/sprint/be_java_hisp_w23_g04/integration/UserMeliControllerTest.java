b4d63e08a0c1c9d25b51b5c70760e02d39f1a893
leivaf <florencia.leiva@mercadolibre.com>
2024-01-03 14:55:04 -0300

    Integration tests added to US-0001

--- 6. sprint II/be_java_hisp_w23_g04_leiva/src/test/java/com/sprint/be_java_hisp_w23_g04/integration/UserMeliControllerTest.java
+++ 6. sprint II/be_java_hisp_w23_g04_leiva/src/test/java/com/sprint/be_java_hisp_w23_g04/integration/UserMeliControllerTest.java
@@ -1,2 +1,145 @@
-package com.sprint.be_java_hisp_w23_g04.integration;public class UserMeliControllerTest {
+package com.sprint.be_java_hisp_w23_g04.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.sprint.be_java_hisp_w23_g04.dto.response.BuyerDTO;
+import com.sprint.be_java_hisp_w23_g04.dto.response.SellerDTO;
+import com.sprint.be_java_hisp_w23_g04.dto.response.SimpleMessageDTO;
+import com.sprint.be_java_hisp_w23_g04.dto.response.UserDTO;
+import com.sprint.be_java_hisp_w23_g04.utils.IntegrationUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserMeliControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    private static ObjectWriter writer;
+
+    @BeforeAll
+    static void initialize() {
+        writer = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .findAndRegisterModules()
+                .writer();
+    }
+
+    /* Tests for followSellerUser() */
+
+    @Test
+    @DisplayName("FollowSellerUser successful integration test")
+    void testFollowSellerUserOk() throws Exception {
+        // params
+        int userId = 5;
+        int userIdToFollow = 8;
+
+        SimpleMessageDTO messageDTO = new SimpleMessageDTO("El usuario con id:" + userId + " ahora sigue a vendedor con id:" + userIdToFollow);
+        String expectedResult = writer.writeValueAsString(messageDTO);
+
+        MvcResult result = this.mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}", userId, userIdToFollow))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andReturn();
+
+        assertEquals(expectedResult, result.getResponse().getContentAsString(StandardCharsets.UTF_8));
+    }
+
+    @Test
+    @DisplayName("FollowSellerUser fails because userId is zero and throw a BadRequestException")
+    void testFollowSellerUserWithUserIdZero() throws Exception {
+        // params
+        int userId = 0;
+        int userIdToFollow = 8;
+
+        SimpleMessageDTO messageDTO = new SimpleMessageDTO(
+                "Se encontraron los siguientes errores en las validaciones:",
+                List.of("El id debe ser mayor a cero"));
+        String expectedResult = writer.writeValueAsString(messageDTO);
+
+        MvcResult result = this.mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}", userId, userIdToFollow))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType("application/json"))
+                .andReturn();
+
+        assertEquals(expectedResult, result.getResponse().getContentAsString(StandardCharsets.UTF_8));
+    }
+
+    @Test
+    @DisplayName("FollowSellerUser fails because userId not exist and throw a NotFoundException")
+    void testFollowSellerUserWhenUserNotExist() throws Exception {
+        // params
+        int userId = 99;
+        int userIdToFollow = 8;
+
+        SimpleMessageDTO messageDTO = new SimpleMessageDTO("No se encontró usuario con el id " + userId + ".");
+        String expectedResult = writer.writeValueAsString(messageDTO);
+
+        MvcResult result = this.mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}", userId, userIdToFollow))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType("application/json"))
+                .andReturn();
+
+        assertEquals(expectedResult, result.getResponse().getContentAsString(StandardCharsets.UTF_8));
+    }
+
+    @Test
+    @DisplayName("FollowSellerUser fails because is the same userId and throw a BadRequestException")
+    void testFollowSellerUserByThemself() throws Exception {
+        // params
+        int userId = 8;
+        int userIdToFollow = 8;
+
+        SimpleMessageDTO messageDTO = new SimpleMessageDTO("El usuario y vendedor con id:" + userId + " no pueden ser el mismo.");
+        String expectedResult = writer.writeValueAsString(messageDTO);
+
+        MvcResult result = this.mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}", userId, userIdToFollow))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType("application/json"))
+                .andReturn();
+
+        assertEquals(expectedResult, result.getResponse().getContentAsString(StandardCharsets.UTF_8));
+    }
+
+    @Test
+    @DisplayName("FollowSellerUser fails because is non-seller userId and throw a BadRequestException")
+    void testFollowNonSellerUser() throws Exception {
+        // params
+        int userId = 5;
+        int userIdToFollow = 4;
+
+        SimpleMessageDTO messageDTO = new SimpleMessageDTO("El id de usuario vendedor proporcionado no es valido.");
+        String expectedResult = writer.writeValueAsString(messageDTO);
+
+        MvcResult result = this.mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}", userId, userIdToFollow))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType("application/json"))
+                .andReturn();
+
+        assertEquals(expectedResult, result.getResponse().getContentAsString(StandardCharsets.UTF_8));
+    }
 }
