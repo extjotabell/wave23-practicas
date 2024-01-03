dd0d44ac8c9a740fbac394328d4fda9594374b45
tvazquezmore <tomas.vazquezmoreyra@mercadolibre.com>
2024-01-03 12:13:52 -0300

    add ignore for more coverage

--- 6. sprint II/be_java_hisp_w23_g04_vazquez/src/main/java/com/sprint/be_java_hisp_w23_g04/BeJavaHispW23G04Application.java
+++ 6. sprint II/be_java_hisp_w23_g04_vazquez/src/main/java/com/sprint/be_java_hisp_w23_g04/BeJavaHispW23G04Application.java
@@ -1,9 +1,11 @@
 package com.sprint.be_java_hisp_w23_g04;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Generated
 public class BeJavaHispW23G04Application {
 
 	public static void main(String[] args) {
