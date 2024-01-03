b477eca4627960a85ed19cef4be8681af04eacc3
ferMezaMel <fernando.mezaortega@mercadolibre.com.mx>
2024-01-03 09:33:39 -0600

    Add test for success follow on valid users

--- 6. sprint II/be_java_hisp_w23_g01_meza/src/test/java/com/meli/socialmeli/integration/UserIntegrationTest.java
+++ 6. sprint II/be_java_hisp_w23_g01_meza/src/test/java/com/meli/socialmeli/integration/UserIntegrationTest.java
@@ -4,10 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,5 +32,18 @@ public class UserIntegrationTest {
                 .withDefaultPrettyPrinter();
     }
 
+    @Test
+    @DisplayName("/users/{userId}/follow/{userIdToFollow} : Usuario logra seguir al otro usuario con éxito")
+    void AddFollowerIntegrationTest() throws Exception{
+        // Arrange & Act & Assert
+        this.mockMvc
+                .perform(post(
+                        "/users/{userId}/follow/{userIdToFollow}", "100", "1100"
+                ))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.message").value("Usuario seguido agregado"));
+    }
 
 }
