61c8324096419f98bd923bd0787fc048281e7aaf
araaquino <arami.aquino@mercadolibre.com>
2024-01-04 18:58:23 -0300

    Finish exercise internet provider

--- 7. bd relacionales/SQL/internet_provider_query.sql
+++ 7. bd relacionales/SQL/internet_provider_query.sql
@@ -1,11 +1,12 @@
 -- EJERCICIO 2
+
 -- a. ¿Cuál es la primary key para la tabla de clientes? Justificar respuesta
 -- DNI, ya que es un identificador único.
 
 -- b. ¿Cuál es la primary key para la tabla de planes de internet? Justificar respuesta.
 -- Creamos plan_id, ya que debemos identificar unequívocamente a cada plan.alter
 
--- ¿Cómo serían las relaciones entre tablas? ¿En qué tabla debería haber foreign key? ¿A qué campo de qué tabla hace referencia dicha foreign key? Justificar respuesta.
+-- c. ¿Cómo serían las relaciones entre tablas? ¿En qué tabla debería haber foreign key? ¿A qué campo de qué tabla hace referencia dicha foreign key? Justificar respuesta.
 -- Entendemos que, 1 cliente puede contratar varios planes, así como 1 plan puede ser contratado a su vez por n clietes
 -- por ende, creamos una tabla intermedia (contract) que contiene a ambos, sumando adicionalmente el campo con la fecha
 -- de contratación.
@@ -13,6 +14,7 @@
 
 
 -- EJERCICIO 3
+
 -- Se solicita crear una nueva base de datos llamada “empresa_internet”. 
 CREATE SCHEMA empresa_internet;
 
@@ -71,15 +73,59 @@ INSERT INTO contrato VALUES (5,2, 56475937, "2022-05-01");
 
 
 -- Plantear 10 consultas SQL que se podrían realizar a la base de datos. Expresar las sentencias.
+
+--  1. Listado de planes con descuento 
+SELECT velocidad, precio FROM plan WHERE descuento IS NOT NULL;
+
+--  2. Listado de clientes de CABA
+SELECT nombre, apellido FROM cliente WHERE provincia LIKE '%CABA%';
+
+-- 3. Listado de clientes con un plan
+SELECT cli.nombre, cli.apellido, p.velocidad FROM cliente cli 
+INNER JOIN contrato con ON cli.dni = con.cliente_dni 
+INNER JOIN plan p ON p.id = con.plan_id
+WHERE con.plan_id IS NOT NULL;
+
+-- 4. Listado de clientes con plan activo desde 2021-05-30
 SELECT cli.nombre, cli.apellido FROM cliente cli 
 INNER JOIN contrato con ON cli.dni = con.cliente_dni 
 WHERE con.fecha_contratacion > "2021-05-30";
 
-
+-- 5. Listado de clientes con plan con descuento contratado desde 2021-05-30
 SELECT cli.nombre, cli.apellido, p.descuento FROM cliente cli 
 INNER JOIN contrato con ON cli.dni = con.cliente_dni 
 INNER JOIN plan p ON p.id = con.plan_id
 WHERE con.fecha_contratacion > "2021-05-30" AND p.descuento IS NOT NULL;
 
+-- 6. Listado ordenado de clientes por fecha de contatación de plan
+SELECT cli.nombre, cli.apellido, con.fecha_contratacion FROM cliente cli 
+INNER JOIN contrato con ON cli.dni = con.cliente_dni 
+WHERE con.plan_id IS NOT NULL
+ORDER BY con.fecha_contratacion DESC;
 
+-- 7. Listado de planes mayores a 100 mb
+SELECT velocidad, precio FROM plan
+WHERE velocidad > 100
+ORDER BY velocidad;
 
+-- 8. Listado de clientes menores de 30 años
+SELECT nombre, apellido, YEAR(CURDATE()) - YEAR(fecha_nacimiento) AS edad 
+FROM cliente 
+WHERE YEAR(CURDATE()) - YEAR(fecha_nacimiento) < 30;
+
+-- 9. Listado de clientes de 30 años o mas
+SELECT nombre, apellido, YEAR(CURDATE()) - YEAR(fecha_nacimiento) AS edad 
+FROM cliente 
+WHERE YEAR(CURDATE()) - YEAR(fecha_nacimiento) >= 30;
+
+-- 10. Plan con mayor precio
+SELECT velocidad, precio 
+FROM plan 
+ORDER BY precio DESC
+LIMIT 1;
+
+-- 11. Plan con menor precio
+SELECT velocidad, precio 
+FROM plan 
+ORDER BY precio ASC
+LIMIT 1;
