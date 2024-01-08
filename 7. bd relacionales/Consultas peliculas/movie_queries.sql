f78d81f241e525cbd6fb2619f82e88981182c2b8
dianisrmz <diana.ramirezgarcia@mercadolibre.com.mx>
2024-01-08 10:08:41 -0600

    (Feat): Consultas avanzadas joins

--- 7. bd relacionales/Consultas peliculas/movie_queries.sql
+++ 7. bd relacionales/Consultas peliculas/movie_queries.sql
@@ -51,18 +51,44 @@ ORDER BY rating DESC
 LIMIT 3;
 
 /* Consultas avanzadas*/
+-- Mostrar el título y el nombre del género de todas las series.
 SELECT series.title, genres.name 
 FROM series INNER JOIN genres
 ON series.genre_id = genres.id;
 
 USE movies_db;
 
+-- Mostrar el título de los episodios, el nombre y apellido de los actores que trabajan en cada uno de ellos.
 SELECT title, first_name, last_name
 FROM actors INNER JOIN
 actor_episode 
 ON actor_episode.actor_id = actors.id
 INNER JOIN episodes 
 ON actor_episode.episode_id = episodes.id;
+
+-- Mostrar el título de todas las series y el total de temporadas que tiene cada una de ellas.
+SELECT series.title, count(seasons.number) as temporadas
+FROM series INNER JOIN
+seasons
+ON series.id = seasons.serie_id
+group by series.title;
+
+-- Mostrar el nombre de todos los géneros y la cantidad total de películas por cada uno, siempre que sea mayor o igual a 3.
+SELECT genres.name, count(movies.genre_id) as peliculas
+FROM genres INNER JOIN
+movies
+ON genres.id = movies.genre_id
+group by genres.name
+having peliculas >2;
+
+-- Mostrar sólo el nombre y apellido de los actores que trabajan en todas las películas de la guerra de las galaxias y que estos no se repitan.
+SELECT DISTINCT first_name, last_name, title
+FROM actor_movie INNER JOIN
+actors
+ON actor_movie.actor_id = actors.id
+INNER JOIN movies
+ON actor_movie.movie_id = movies.id
+WHERE movies.title like "La Guerra de las galaxias%";
 
 /*Consultas avanzadas 2*/
 
