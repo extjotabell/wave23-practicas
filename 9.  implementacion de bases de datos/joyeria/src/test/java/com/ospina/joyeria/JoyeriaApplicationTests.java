c6548b160705c58d4b1936902a160647e9f58510
Sebastian Ospina Garcia <sebastian.ospina@mercadolibre.com.co>
2024-01-10 15:15:05 -0500

    (Chore): Se creó la base del proyecto de la joyeria

---
+++ 9.  implementacion de bases de datos/joyeria/src/test/java/com/ospina/joyeria/JoyeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ospina.joyeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
