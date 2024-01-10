60f375382a407ebc34e104a2ab5e8c91e7877e68
ailenvilches <ailen.pereira@mercadolibre.com>
2024-01-10 16:15:35 -0300

    Proyecto inicial implementación bd

---
+++ 9.  implementacion de bases de datos/PruebaH2/src/main/java/spring/pereiravilches/PruebaH2/PruebaH2Application.java
@@ -0,0 +1,13 @@
+package spring.pereiravilches.PruebaH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaH2Application.class, args);
+	}
+
+}
---
+++ 9.  implementacion de bases de datos/PruebaH2/src/test/java/spring/pereiravilches/PruebaH2/PruebaH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.pereiravilches.PruebaH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
