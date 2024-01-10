8b8bb0d51861725cd2b752ad287cfb23f0210a56
Sebastian Ospina Garcia <sebastian.ospina@mercadolibre.com.co>
2024-01-10 16:00:09 -0500

    (Feat): Se creó método de obtener todas las joyas

--- 9.  implementacion de bases de datos/joyeria/src/main/java/com/ospina/joyeria/controlador/JoyaControlador.java
+++ 9.  implementacion de bases de datos/joyeria/src/main/java/com/ospina/joyeria/controlador/JoyaControlador.java
@@ -1,13 +1,13 @@
 package com.ospina.joyeria.controlador;
 
+import com.ospina.joyeria.modelo.entidad.Joya;
 import com.ospina.joyeria.modelo.request.JoyaRequestDTO;
 import com.ospina.joyeria.servicio.JoyaServicio;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/jewelry")
@@ -23,5 +23,10 @@ public class JoyaControlador {
     public ResponseEntity<String> crearJoya(@RequestBody JoyaRequestDTO joyaRequestDTO) {
         Long id = joyaServicio.guardarJoya(joyaRequestDTO);
         return new ResponseEntity<>("Joya creada con el ID: " + id, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Joya>> obtenerJoyas() {
+        return new ResponseEntity<>(joyaServicio.obtenerJoyas(), HttpStatus.OK);
     }
 }
