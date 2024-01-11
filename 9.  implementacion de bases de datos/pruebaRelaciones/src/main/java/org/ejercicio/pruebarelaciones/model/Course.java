18f5df5d9703aa6907ac129113f782c64ecfd0f3
tvazquezmore <tomas.vazquezmoreyra@mercadolibre.com>
2024-01-11 15:35:09 -0300

    add example NtoN

---
+++ 9.  implementacion de bases de datos/pruebaRelaciones/src/main/java/org/ejercicio/pruebarelaciones/model/Course.java
@@ -0,0 +1,17 @@
+package org.ejercicio.pruebarelaciones.model;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToMany(mappedBy = "likedCourses")
+    private Set<Student> likes;
+}
---
+++ 9.  implementacion de bases de datos/pruebaRelaciones/src/main/java/org/ejercicio/pruebarelaciones/model/Student.java
@@ -0,0 +1,22 @@
+package org.ejercicio.pruebarelaciones.model;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToMany
+    @JoinTable(
+            name = "couse_like",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id")
+    )
+    private Set<Course> likedCourses;
+}
