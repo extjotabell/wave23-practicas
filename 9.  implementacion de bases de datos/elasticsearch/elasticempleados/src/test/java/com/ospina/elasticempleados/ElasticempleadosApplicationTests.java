b93704c94898ef5bbec90a9d3c3128df0992f204
Sebastian Ospina Garcia <sebastian.ospina@mercadolibre.com.co>
2024-01-12 12:14:10 -0500

    (Chore): Se creó la base para el ejercicio de Elastic Search con Empleados

---
+++ 9.  implementacion de bases de datos/elasticsearch/elasticempleados/src/test/java/com/ospina/elasticempleados/ElasticempleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ospina.elasticempleados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticempleadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
