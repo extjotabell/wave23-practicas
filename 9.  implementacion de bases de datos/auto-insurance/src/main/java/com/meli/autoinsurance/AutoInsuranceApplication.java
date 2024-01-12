12be886fac28ce411957dab6525a91c31266dde6
zulyvargasrz <zuly.vargasramirez@mercadolibre.com.co>
2024-01-12 12:45:43 -0500

    Add base for AutoInsurance HQL practice

---
+++ 9.  implementacion de bases de datos/auto-insurance/src/main/java/com/meli/autoinsurance/AutoInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.meli.autoinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoInsuranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoInsuranceApplication.class, args);
+    }
+
+}
---
+++ 9.  implementacion de bases de datos/auto-insurance/src/test/java/com/meli/autoinsurance/AutoInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.autoinsurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoInsuranceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
