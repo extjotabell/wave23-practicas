3459f51a6c425eb1c1a07242c32c315c324cb692
miguelOchoaM <miguel.ochoa@mercadolibre.com.co>
2024-01-12 13:36:07 -0500

    Modificando los DTOs mapper acorde a los nuevos DTOs

--- 9.  implementacion de bases de datos/Seguros/src/main/java/com/example/seguros/util/DTOMapper.java
+++ 9.  implementacion de bases de datos/Seguros/src/main/java/com/example/seguros/util/DTOMapper.java
@@ -9,19 +9,21 @@ import com.example.seguros.model.Vehicle;
 
 public class DTOMapper {
     public static Vehicle mapToVehicle(VehicleRequestDTO vehicleRequestDTO) {
-        return new Vehicle(vehicleRequestDTO.getBrand());
+        return new Vehicle(vehicleRequestDTO.getPatent(), vehicleRequestDTO.getBrand(), vehicleRequestDTO.getModel(),
+                vehicleRequestDTO.getYearOfFabrication(), vehicleRequestDTO.getWheels());
     }
 
     public static VehicleResponseDTO mapToVehicleResponseDTO(Vehicle vehicle) {
-        return new VehicleResponseDTO(vehicle.getId(), vehicle.getBrand());
+        return new VehicleResponseDTO(vehicle.getId(), vehicle.getPatent(), vehicle.getBrand(), vehicle.getModel(),
+                vehicle.getYearOfFabrication(), vehicle.getWheels());
     }
 
     public static Accident mapToAccident(AccidentRequestDTO accidentRequestDTO, Vehicle vehicle) {
-        return new Accident(accidentRequestDTO.getDate(), vehicle);
+        return new Accident(accidentRequestDTO.getDate(), accidentRequestDTO.getCost(), vehicle);
     }
 
     public static AccidentResponseDTO mapToAccidentResponseDTO(Accident accident) {
-        return new AccidentResponseDTO(accident.getId(), accident.getDate(),
+        return new AccidentResponseDTO(accident.getId(), accident.getDate(), accident.getCost(),
                 mapToVehicleResponseDTO(accident.getVehicle()));
     }
 }
