99fd9c8f817437674fa8e12f92cb4f192d283851
miguelOchoaM <miguel.ochoa@mercadolibre.com.co>
2024-01-12 14:12:02 -0500

    Agregando las consultas solicitadas en los repositorios

--- 9.  implementacion de bases de datos/Seguros/src/main/java/com/example/seguros/repository/VehicleRepository.java
+++ 9.  implementacion de bases de datos/Seguros/src/main/java/com/example/seguros/repository/VehicleRepository.java
@@ -2,6 +2,21 @@ package com.example.seguros.repository;
 
 import com.example.seguros.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    @Query("SELECT v.patent FROM Vehicle v")
+    List<String> findAllPatents();
+
+    @Query("SELECT v.patent, v.brand FROM Vehicle v ORDER BY v.yearOfFabrication")
+    List<Object[]> findAllPatentsAndBrand();
+
+    @Query("SELECT v FROM Vehicle v WHERE v.wheels > 4 AND YEAR(v.yearOfFabrication) = :currentYear")
+    List<Vehicle> findAllByCurrentYear(@Param("currentYear") int currentYear);
+
+    @Query("SELECT a.vehicle FROM Accident a WHERE a.cost > :cost")
+    List<Vehicle> findAllByCostGreaterThan(@Param("cost") Double cost);
 }
