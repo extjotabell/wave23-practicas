9bd717fda53fbc9ee63e956d4af581d69270df5b
miguelOchoaM <miguel.ochoa@mercadolibre.com.co>
2024-01-12 14:11:37 -0500

    Agregando los endpoints solicitados

--- 9.  implementacion de bases de datos/Seguros/src/main/java/com/example/seguros/controller/Controller.java
+++ 9.  implementacion de bases de datos/Seguros/src/main/java/com/example/seguros/controller/Controller.java
@@ -6,9 +6,7 @@ import com.example.seguros.service.accident.AccidentService;
 import com.example.seguros.service.vehicle.VehicleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
@@ -30,5 +28,35 @@ public class Controller {
     public ResponseEntity<?> postVehicle(@RequestBody VehicleRequestDTO vehicleRequestDTO) {
         vehicleService.postVehicle(vehicleRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/accident")
+    public ResponseEntity<?> findAllAccident() {
+        return ResponseEntity.status(HttpStatus.OK).body(accidentService.findAll());
+    }
+
+    @GetMapping("/vehicle")
+    public ResponseEntity<?> findAllVehicle() {
+        return ResponseEntity.status(HttpStatus.OK).body(vehicleService.findAll());
+    }
+
+    @GetMapping("/vehicle/patents")
+    public ResponseEntity<?> findAllPatentsVehicle() {
+        return ResponseEntity.status(HttpStatus.OK).body(vehicleService.findAllPatents());
+    }
+
+    @GetMapping("/vehicle/patentsAndBrands")
+    public ResponseEntity<?> findAllPatentsAndBrandVehicle() {
+        return ResponseEntity.status(HttpStatus.OK).body(vehicleService.findAllPatentsAndBrand());
+    }
+
+    @GetMapping("/vehicle/currentYear")
+    public ResponseEntity<?> findAllByCurrentYear() {
+        return ResponseEntity.status(HttpStatus.OK).body(vehicleService.findAllByCurrentYear());
+    }
+
+    @GetMapping("/vehicle/findAllByCostGreaterThan/{cost}")
+    public ResponseEntity<?> findAllByCostGreaterThan(@PathVariable Double cost) {
+        return ResponseEntity.status(HttpStatus.OK).body(vehicleService.findAllByCostGreaterThan(cost));
     }
 }
