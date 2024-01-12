829fdc5798086ad2ba2ff52b17080c0d4a009ad0
leivaf <florencia.leiva@mercadolibre.com>
2024-01-12 17:44:03 -0300

    Add project with theory explanation view in live coding

---
+++ 9.  implementacion de bases de datos/obras-literarias/src/main/java/com/example/clasenosql/ClasenosqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.clasenosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasenosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasenosqlApplication.class, args);
+	}
+
+}
